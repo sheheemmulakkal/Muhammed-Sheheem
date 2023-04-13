169a292fe4311943ee865ecfb1df3edc09bdd83d
Muhammed Sheheem <shaimonsheheem@gmail.com>
2023-04-13 12:36:19 +0530

    almost completed...validation and submission completed

---
+++ forms/contact.js
@@ -0,0 +1,54 @@
+function sendMail(){
+
+    
+
+    let status = sendMessage()
+    if(status){
+
+        var params = {
+            name : document.getElementById("name").value,
+            email : document.getElementById("email").value,
+            subject : document.getElementById("subject").value,
+            message : document.getElementById("message").value
+    
+        }
+        
+        const serviceID = "service_1laz7m6" ;
+        const templateID = "template_zvpb1br";
+        
+        emailjs.send(serviceID,templateID,params).then((res)=>{
+            document.getElementById("name").value ="";
+            document.getElementById("email").value ="";
+            document.getElementById("subject").value ="";
+            document.getElementById("message").value =""; 
+            console.log(res);
+            alert("Submitted successfully");
+        }).catch((err)=>{
+            console.log(err);
+        });
+    }
+
+
+}
+
+
+function sendMessage(){
+    let btn = true
+var name=document.getElementById("name").value.trim()
+var email=document.getElementById("email").value.trim()
+var subject=document.getElementById('subject').value.trim()
+var message=document.getElementById("message").value.trim()
+
+if(name===""||email===""||subject===""||message==="")
+{
+    alert("Please fill all fields")
+    btn=false
+}else if(!/^([a-zA-Z0-9\_.]+)@([a-zA-Z0-9]+).([a-z]+).([a-z]+)?$/.test(email)){
+    alert("Please re-check at email")
+    btn=false
+ }
+
+return btn;
+
+}
+
